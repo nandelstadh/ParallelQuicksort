ea980c71948d777e5d29c18443327f0e4aee8e52
Anton von Nandelstadh <anton.von.nandelstadh@gmail.com>
2026-03-12 17:01:15 +0100

    First working implementation of quicksort.c

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC = gcc-15
 LD = gcc-15
-CFLAGS = -O3 -Wall -Werror -fopenmp -fsanitize=address
+CFLAGS = -O3 -Wall -fopenmp -g
 LDFLAGS = -fopenmp
 RM = /bin/rm -f
 OBJS = main.o quicksort.o
--- quicksort.c
+++ quicksort.c
@@ -16,19 +16,127 @@ int compare(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
 
 void seqSort(int arr[], int N) { qsort(arr, N, sizeof(int), compare); }
 
-void gsortHelper(int arr[], int N, int n_threads, int depth) {
-    printf("We got into the helper function");
-    printf("I know what my ID is, it is: %d \n", omp_get_thread_num());
-    // a. Select pivot
-#pragma omp barrier
-    // b. Divide into smaller and larger
-    // c. Split processors into two groups and exchange data pairwise
-    // d. Merge data into sorted list
+// Sets each pivot to the median of the data in the first processor of each
+// group. Should be optimized later with a better algorithm maybe
+void pivotFinderA(int group_id, int arr[], int localLen, int pivots[]) {
+    pivots[group_id] = arr[localLen / 2];
+    printf("We compute the pivot to be: %d\n", pivots[group_id]);
 }
 
+int binarySearch(int arr[], int start, int length, int key) {
+    int left = start;
+    int right = start + length;
+    while (left < right) {
+        int mid = left + ((right - left) / 2);
+
+        if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return left;
+}
+
+void mergeData(int* localLists[], int localLen[], int m_buf[], int left_list[], int left_len, int r_buf[], int r_buf_len, int id) {
+    localLen[id] = left_len + r_buf_len;
+
+    int a = 0;
+    int b = 0;
+    int comp = 0;
+
+    for (int i = 0; i < localLen[id]; i++) {
+        if (a >= left_len)
+            m_buf[i] = r_buf[b++];
+
+        else if (b >= r_buf_len)
+            m_buf[i] = left_list[a++];
+
+        else if (left_list[a] <= r_buf[b])
+            m_buf[i] = left_list[a++];
+
+        else
+            m_buf[i] = r_buf[b++];
+    }
+}
+
+void gsortHelper(int* localLists[], int localLen[], int N, int n_threads, int iter) {
+    int group_size = n_threads >> iter;
+    int group_num = n_threads / group_size;
+    int pivots[group_num];
+    long partitions[n_threads];
+
+#pragma omp parallel num_threads(n_threads)
+    {
+        int id = omp_get_thread_num();
+        int group_id = id / group_size;
+        int group_start = group_id * group_size;
+        // a. Select pivot
+        if (id % group_size == 0)
+            pivotFinderA(group_id, localLists[id], localLen[id], pivots);
+
+#pragma omp barrier
+        // b. Divide into smaller and larger
+        partitions[id] = binarySearch(localLists[id], 0, localLen[id], pivots[group_id]);
+
+        // c. Split processors into two groups and exchange data pairwise
+        int pair_id = group_start + ((id - group_start + (group_size >> 1)) % group_size);
+
+        // Part d. Merge data from pair into sorted list
+        int* r_buf = malloc(N * sizeof(int));
+        int* m_buf = malloc(N * sizeof(int));
+
+#pragma omp barrier
+        printf("Group_num: %d; Group_size: %d; Group_id: %d; Group_start: %d; Id: %d; Pair_Id: %d\n", group_num, group_size, group_id, group_start, id, pair_id);
+        /* printf("The pivot is %d: \n", pivots[group_id]); */
+        /* printf("Here we have partition of id %d and pair_id %d: %ld and %ld\n ", id, pair_id, partitions[id], partitions[pair_id]); */
+
+// This barrier is only for debugging
+#pragma omp barrier
+
+        int r_buf_len;
+        int left_len;
+        int* left_list;
+        if (id < pair_id) {
+            // We receive lower list
+            left_list = localLists[id];
+            r_buf_len = partitions[pair_id];
+            left_len = partitions[id];
+            /* printf("Before memcpy lower lists \n"); */
+            memcpy(r_buf, (int*)localLists[pair_id], r_buf_len * sizeof(int));
+            /* printf("After memcpy lower lists \n"); */
+        } else {
+            // We receive higher list
+            left_list = localLists[id] + partitions[id];
+            r_buf_len = localLen[pair_id] - partitions[pair_id];
+            left_len = localLen[id] - partitions[id];
+            /* printf("Before memcpy higher lists \n"); */
+            memcpy(r_buf, (int*)localLists[pair_id] + partitions[pair_id], r_buf_len * sizeof(int));
+            /* printf("After memcpy higher lists \n"); */
+        }
+
+        /* printf("Finished sending lists back and fort \n"); */
+#pragma omp barrier
+        // Finally, we merge the data into our merger buffer
+        /* printf("This is before merging data\n"); */
+        mergeData(localLists, localLen, m_buf, left_list, left_len, r_buf, r_buf_len, id);
+        /* printf("This is after merging data\n"); */
+        memcpy(localLists[id], m_buf, localLen[id] * sizeof(int));
+        free(r_buf);
+        free(m_buf);
+    }
+}
+
+/* void gsort(int arr[], int N, int n_threads) { */
+/*     seqSort(arr, N); */
+/* } */
 void gsort(int arr[], int N, int n_threads) {
     int block_size = N / n_threads;
     int remainder = N % n_threads;
+    int* localLists[n_threads];
+    int localLen[n_threads];
+
 #pragma omp parallel num_threads(n_threads)
     {
         int id = omp_get_thread_num();
@@ -40,13 +148,31 @@ void gsort(int arr[], int N, int n_threads) {
             start = id * block_size + remainder;
             length = block_size;
         }
-        int* localList = malloc(length * sizeof(int));
-        memcpy(localList, arr + start, length * sizeof(int));
-        seqSort(localList, length);
 
-        for (int i = 0; i < log2(n_threads); i++) {
-            gsortHelper(localList, length, n_threads, i);
-        }
-        free(localList);
+        int* localList = malloc(N * sizeof(int));
+        memcpy(localList, arr + start, length * sizeof(int));
+        localLists[id] = localList;
+        localLen[id] = length;
+        seqSort(localList, length);
+    }
+    for (int i = 0; i < n_threads; i++) {
+        printf("These are the intial lengths of the lists: %d\n", localLen[i]);
+    }
+
+    for (int i = 0; i < log2(n_threads); i++) {
+        gsortHelper(localLists, localLen, N, n_threads, i);
+    }
+
+    int location = 0;
+    for (int i = 0; i < n_threads; i++) {
+        memcpy(arr + location, localLists[i], localLen[i] * sizeof(int));
+        location += localLen[i];
+    }
+
+//  Freeing the local lists
+#pragma omp parallel num_threads(n_threads)
+    {
+        int id = omp_get_thread_num();
+        free(localLists[id]);
     }
 }
